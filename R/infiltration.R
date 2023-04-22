bf741f1fa56b73b26b8ca3efd6a0a57d5ac9827f
Saryace <saryace@gmail.com>
2023-04-21 21:44:44 -0400

    add roxygen for infiltration

--- R/infiltration.R
+++ R/infiltration.R
@@ -1,4 +1,16 @@
-library(tidyverse)
+#' Cumulative infiltration and sqrt of time
+#'
+#' Using
+#'
+#' @return A tibble giving three new columns: sqrt_time,
+#'    volume_infiltrated and infiltration
+#' @export
+#' @examples
+#' infiltration(tibble(time = c(0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300,
+#' 0, 35, 65, 95, 125, 155, 185, 215, 245, 275, 305),
+#' volume = c(95, 89, 86, 83, 80, 77, 74, 73, 71, 69, 67,
+#'            83, 77, 64, 61, 58, 45, 42, 35, 29, 17, 15)))
+
 
 infiltration <- function(dataset, col_name, ...) {
   dataset %>%
