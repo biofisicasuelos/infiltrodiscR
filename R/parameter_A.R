769ae4839d3c3e09bb8e6cc35bceaf0c0b7cddad
Saryace <saryace@gmail.com>
2024-05-19 21:07:53 -0400

    add changes parameter A

--- R/parameter_A.R
+++ R/parameter_A.R
@@ -14,7 +14,7 @@ parameter_A <- function(dataset, col_name, ...) {
   dataset$suction_num <- -as.numeric(gsub("[^0-9.-]", "", dataset$suction))
 
   # Calculate parameter_A based on n value over or under 1.9
-  dataset$parameter_A <- (11.65 * (dataset$n_ho^(0.1) - 1) * exp(ifelse(dataset$n_ho < 1.9, 7.5, 2.92) * (dataset$n_ho - 1.9) * alpha * dataset$suction_num)) / ((alpha * 2.25)^(0.91))
+  dataset$parameter_A <- (11.65 * (dataset$n_ho^(0.1) - 1) * exp(ifelse(dataset$n_ho < 1.9, 7.5, 2.92) * (dataset$n_ho - 1.9) * dataset$alpha * dataset$suction_num)) / ((dataset$alpha * 2.25)^(0.91))
 
   return(dataset)
 }
